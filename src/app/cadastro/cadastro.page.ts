130695c61a7693e24488acfc42d4bb345db758cf
Filipe <filipecbs94@gmail.com>
2019-07-30 15:34:50 -0300

    + Catch e Cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.page.html',
-  styleUrls: ['./cadastro.page.scss'],
+    selector: 'app-cadastro',
+    templateUrl: './cadastro.page.html',
+    styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+    registerForm: FormGroup;
 
-  ngOnInit() {
-  }
+    radiosArray = ["Bulbasaur", "Charmander", "Squirtle"];
+    constructor(public formbuilder: FormBuilder, public toastController: ToastController) {
+
+        this.registerForm = this.formbuilder.group({
+            name: [null, [Validators.required, Validators.minLength(3)]],
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, [Validators.required, Validators.minLength(6)]],
+            phone: [null, [Validators.required]],
+            starterPokemon: [null, [Validators.required]]
+        });
+
+    }
+
+    ngOnInit() {
+    }
+
+    async presentToast() {
+        const toast = await this.toastController.create({
+            message: 'Treinador cadastrado.',
+            duration: 2000
+        });
+        toast.present();
+    }
+
+    submitForm(form) {
+        console.log(form);
+        console.log(form.value);
+        this.presentToast();
+    }
 
 }
--- src/app/pokemons/pokemons.page.ts
+++ src/app/pokemons/pokemons.page.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../service/pokemon.service';
 
 @Component({
-  selector: 'app-pokemons',
-  templateUrl: './pokemons.page.html',
-  styleUrls: ['./pokemons.page.scss'],
+    selector: 'app-pokemons',
+    templateUrl: './pokemons.page.html',
+    styleUrls: ['./pokemons.page.scss'],
 })
 export class PokemonsPage implements OnInit {
 
-  constructor() { }
+    time:number = 5;
+    pokemons = [];
 
-  ngOnInit() {
-  }
+    constructor(private pokemonService: PokemonService) { }
 
+    ngOnInit() {
+    }
+
+    catchPokemon() {
+        this.waitCounter();
+        this.pokemonService.getPokemon(this.getRandomPokemonID()).subscribe(
+            (res) => {
+                console.log(res);
+                this.pokemons.push(res);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    getRandomPokemonID() {
+        return Math.floor(Math.random() * 807) + 1;
+    }
+
+    waitCounter() {
+        const interval = setInterval(() => {
+
+            this.time--;
+
+            if (this.time === 0) {
+                this.time = 5;
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
+
+    goToPokemonProfile(id) {
+        console.log('Ir para o perfil do pokémon de id', id);
+    }
 }
